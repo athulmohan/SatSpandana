06bac22bfcd6e5349d69485254ef48bc5f9af6ed
Athul Mohan C <athul.mhn@gmail.com>
2025-04-13 16:49:15 +0530

    slot add

--- hms/include/book_slot.php
+++ hms/include/book_slot.php
@@ -9,17 +9,28 @@
     if (isset($_GET['date'])) {
         $date = $_GET['date'];
         $selectedDoc = $_GET['doc'];
-        $sql = "SELECT slot_time FROM booking_slots 
-            WHERE booking_date = '$date' 
-            AND slot_time NOT IN (SELECT appointmentTime FROM appointment WHERE appointmentDate = '$date' AND doctorId = '$selectedDoc' AND userStatus = 1 AND doctorStatus = 1)";
-        $result = $con->query($sql);
-        $slots = [];
+        $selectedLoc = $_GET['loc'];
 
-        while ($row = $result->fetch_assoc()) {
-            $slots[] = $row['slot_time'];
-        }
-        echo json_encode($slots);
-        exit;
+        // $checkkSql = "SELECT count(*) as count FROM appointment WHERE appointmentDate = '$date' AND doctorId = '$selectedDoc' AND locationId = '$selectedLoc' AND userStatus = 1 AND doctorStatus = 1";
+        // $checkResult = $con->query($checkkSql);
+        // $row = mysqli_fetch_assoc($checkResult);
+
+        // if (isset($row['count']) && $row['count'] > 0) {
+        //    echo json_encode(['status' => 'error', 'message' => 'Already booked']);
+        //     exit; 
+        // } else {
+            $sql = "SELECT slot_time FROM booking_slots 
+                WHERE booking_date = '$date' AND doctor_id = '$selectedDoc' AND location_id = '$selectedLoc'
+                AND slot_time NOT IN (SELECT appointmentTime FROM appointment WHERE appointmentDate = '$date' AND doctorId = '$selectedDoc' AND locationId = '$selectedLoc' AND userStatus = 1 AND doctorStatus = 1)";
+            $result = $con->query($sql);
+            $slots = [];
+
+            while ($row = $result->fetch_assoc()) {
+                $slots[] = $row['slot_time'];
+            }
+            echo json_encode($slots);
+            exit;
+        // }
     }
 // }
 ?>
